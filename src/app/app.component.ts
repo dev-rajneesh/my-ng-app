2ca8a777fd694718bd3d0d85979f60ef186694aa
Abhishek <vyasabhishek0606@outlook.com>
2020-02-16 13:59:23 +0530

    Session 9: 16 Feb Angular 1st App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+// selector: Custom HTML tag that will be used to refer and load component in DOM
+// templateUrl: the external HTML file that contains UI for Component
+// template: an inline HTML that contains UI for Component
+// style: Inline CSS
+// styleUrls: External CSS files
+
 @Component({
-  selector: 'app-root',
+  // selector: 'app-root',
+  selector: 'app-app-component',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-ng-app';
+  message: string;
+  name: string;
+  categories: Array<any>;
+  names: Array<string>;
+  selectedName: string;
+  selectedCat: any;
+
+  constructor(){
+    this.message = "My name is Angular";
+    this.name = "";
+    this.names = ['Rajneesh','Monika', 'Sudanta', 'Prasanna'];
+    this.categories = [
+      {CatId: 101, CatName: 'ECT'},
+      {CatId: 102, CatName: 'ECL'},
+      {CatId: 103, CatName: 'FOD'}
+    ];
+    this.selectedName = '';
+    this.selectedCat = {};
+  }
+
+  display(): void{
+    this.message = "My name is Angular 9";
+  }
+
+  acceptValue(evt) : void{
+    this.message += evt.target.value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,35 @@
+// BrowserModule, loads the APpModule contents in the browser and render it
 import { BrowserModule } from '@angular/platform-browser';
+// NgModule the class decorator to define class as Angular Module
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+// Custom Module for containing Routing Object Model
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// imports: of type array, used to import and load standard Angular Modules and 
+// external modules in current application
+
+// declarations: of the type array, used to declare instances of all componenets
+// directives and pipes of this application in current module
+
+// pipes: of the type array, used to register angular service in DI container at Module level
+
+// bootstrap: of the type array, used to bootstrap one of more component in browser
+// when AppModule is loaded
+
+// exports: array type, used to export components/services/directives/pipes from current application to other
+// angular application
+
+// entryComponent: Components not bootstrapped but used by bootstraped components to load
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, FormsModule,
     AppRoutingModule
   ],
   providers: [],
